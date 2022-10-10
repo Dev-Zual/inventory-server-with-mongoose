dc1e48a242f6ffd092dcd00863f700e0501d5908
Zual Rana <freelancerzual@gmail.com>
2022-10-10 12:50:04 +0600

    inventory server with mongoose

--- app.js
+++ app.js
@@ -1,16 +1,93 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 
-
+// middleware
 app.use(express.json());
 app.use(cors());
 
-
+// schema design
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name for this product."],
+      trim: true, // unwanted spaces
+      unique: [true, "Name must be unique."],
+      minLength: [3, "Name must be at least 3 characters."],
+      maxLength: [100, "Name is too large."],
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, "Price can't be negative."],
+    },
+    unit: {
+      type: String,
+      required: true,
+      enum: {
+        value: ["kg", "litre", "pcs"],
+        message: "unit value can't be {VALUE}, must be kg/litre/pcs",
+      },
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "quantity can't be negative."],
+      validate: {
+        validator: (value) => {
+          const isInteger = Number.isInteger(value);
+          if (isInteger) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      message: "quantity must be an integer.",
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        value: ["in-stock", "out-of-stock", "discontinued"],
+        message: "status can't be {VALUE}",
+      },
+    },
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
+    // updatedAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
+    supplier: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "supplier",
+    },
+    categories: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        _id: mongoose.Schema.Types.ObjectId,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
-
 
 module.exports = app;
